7a187d4baa98f7209ae134c246b77a0c925e43c0
CyberSleeper <mahartha.gemilang@gmail.com>
2023-02-28 13:06:34 +0700

    changed img to Image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 function Navbar() {
   return(
   <nav className="flex justify-between m-5 mr-10 ml-0 items-center">
     <Link href="/">
-      <img src="/NavbarLogo.svg" className="w-[230px] h-[50px]"/>
+      <Image
+        src="/NavbarLogo.svg"
+        alt="NavbarLogo"
+        width={230}
+        height={50}
+      />
     </Link>
     <div className="flex gap-4 items-center">
-      <img src="/anonymous.png" className="w-[50px] rounded-full"/>
+      <Image
+        className='rounded-full'
+        src="/anonymous.png"
+        alt="ProfilePicture"
+        width={50}
+        height={50}
+      />
       <h1>Anonymous</h1>
     </div>
   </nav>
